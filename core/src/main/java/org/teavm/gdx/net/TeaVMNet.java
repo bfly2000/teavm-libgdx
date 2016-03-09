98bdcaadf32e339daeaabb031a4c5a693f7a66fd
MJ <john.hervicc@gmail.com>
2016-03-09 12:28:40 +0100

    Implemented TeaVMNet.

--- core/src/main/java/org/teavm/gdx/net/TeaVMNet.java
+++ core/src/main/java/org/teavm/gdx/net/TeaVMNet.java
@@ -1,6 +1,11 @@
 
 package org.teavm.gdx.net;
 
+import java.util.Map.Entry;
+
+import org.teavm.jso.JSBody;
+import org.teavm.jso.ajax.ReadyStateChangeHandler;
+import org.teavm.jso.ajax.XMLHttpRequest;
 import org.teavm.jso.browser.Window;
 
 import com.badlogic.gdx.Net;
@@ -8,11 +13,16 @@ import com.badlogic.gdx.net.ServerSocket;
 import com.badlogic.gdx.net.ServerSocketHints;
 import com.badlogic.gdx.net.Socket;
 import com.badlogic.gdx.net.SocketHints;
+import com.badlogic.gdx.utils.GdxRuntimeException;
+import com.badlogic.gdx.utils.ObjectMap;
 
 /** {@link Net} for TeaVM backend. Opens URIs in a separate tab. Allows to send and cancel HTTP request. Does not support TCP
- * server or client sockets. If you need a more robust networking solution, do some research on web sockets.
+ * server or client sockets. If you need a more robust networking solution, try doing research on web sockets.
  * @author MJ */
 public class TeaVMNet implements Net {
+	private final ObjectMap<HttpRequest, XMLHttpRequest> requests = new ObjectMap<>();
+	private final ObjectMap<HttpRequest, HttpResponseListener> listeners = new ObjectMap<>();
+
 	@Override
 	public boolean openURI (final String URI) {
 		Window.current().open(URI, "_blank");
@@ -21,13 +31,63 @@ public class TeaVMNet implements Net {
 
 	@Override
 	public void sendHttpRequest (final HttpRequest httpRequest, final HttpResponseListener httpResponseListener) {
-		// TODO send HTTP request
+		if (httpRequest.getUrl() == null || httpRequest.getMethod() == null || httpResponseListener == null) {
+			httpResponseListener.failed(new GdxRuntimeException("Unable to send request without URL, method or listener."));
+			return;
+		}
+		final XMLHttpRequest request = XMLHttpRequest.create();
+		request.setOnReadyStateChange(new ReadyStateChangeHandler() {
+			@Override
+			public void stateChanged () {
+			if (request.getReadyState() != XMLHttpRequest.DONE) {
+				return;
+			}
+			httpResponseListener.handleHttpResponse(new TeaVMHttpResponse(request));
+			requests.remove(httpRequest);
+			listeners.remove(httpRequest);
+			}
+		});
+		final boolean valueInUrl = HttpMethods.GET.equalsIgnoreCase(httpRequest.getMethod())
+			|| HttpMethods.DELETE.equalsIgnoreCase(httpRequest.getMethod());
+		for (final Entry<String, String> header : httpRequest.getHeaders().entrySet()) {
+			request.setRequestHeader(header.getKey(), header.getValue());
+		}
+		setTimeOut(request, httpRequest.getTimeOut());
+		setIncludeCredentials(request, httpRequest.getIncludeCredentials());
+		if (valueInUrl) {
+			request.open(httpRequest.getMethod().toUpperCase(), httpRequest.getUrl() + "?" + httpRequest.getContent());
+			request.send();
+		} else {
+			request.open(httpRequest.getMethod().toUpperCase(), httpRequest.getUrl());
+			request.send(httpRequest.getContent());
+		}
 	}
+
+	/** @param request will be modified.
+	 * @param timeOut will be set as "timeout" property. */
+	@JSBody(params = {"request", "timeOut"}, script = "request.timeout=timeOut;")
+	protected native void setTimeOut (XMLHttpRequest request, int timeOut);
+
+	/** @param request will be modified.
+	 * @param includeCredentials true to include credentials. */
+	@JSBody(params = {"request", "includeCredentials"}, script = "request.withCredentials=includeCredentials;")
+	protected native void setIncludeCredentials (XMLHttpRequest request, boolean includeCredentials);
 
 	@Override
 	public void cancelHttpRequest (final HttpRequest httpRequest) {
-		// TODO cancel HTTP request
+		final XMLHttpRequest request = requests.remove(httpRequest);
+		if (request != null) {
+			cancelRequest(request);
+		}
+		final HttpResponseListener listener = listeners.remove(httpRequest);
+		if (listener != null) {
+			listener.cancelled();
+		}
 	}
+
+	/** @param request will be aborted. */
+	@JSBody(params = "request", script = "request.abort();")
+	protected native void cancelRequest (XMLHttpRequest request);
 
 	@Override
 	public ServerSocket newServerSocket (final Protocol protocol, final String hostname, final int port,
